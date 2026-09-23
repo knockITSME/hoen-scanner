78e6a30d4866728d2e27a631fe7f8076e071180f
Mohammed Khan <99343044+knockITSME@users.noreply.github.com>
2026-09-23 13:28:25 +0530

    Implement search API

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,8 +1,14 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -10,7 +16,6 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
         new HoenScannerApplication().run(args);
     }
 
-    @Override
     public String getName() {
         return "hoen-scanner";
     }
@@ -21,8 +26,50 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final HoenScannerConfiguration configuration,
+                    final Environment environment) throws Exception {
 
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<SearchResult> searchResults = new ArrayList<>();
+
+        InputStream hotelsFile = getClass()
+                .getClassLoader()
+                .getResourceAsStream("hotels.json");
+
+        InputStream rentalCarsFile = getClass()
+                .getClassLoader()
+                .getResourceAsStream("rental_cars.json");
+
+        List<SearchResult> hotels = objectMapper.readValue(
+                hotelsFile,
+                new TypeReference<List<SearchResult>>() {}
+        );
+
+        List<SearchResult> rentalCars = objectMapper.readValue(
+                rentalCarsFile,
+                new TypeReference<List<SearchResult>>() {}
+        );
+
+        for (SearchResult hotel : hotels) {
+            searchResults.add(
+                    new SearchResult(
+                            hotel.getCity(),
+                            "hotel",
+                            hotel.getTitle()
+                    )
+            );
+        }
+
+        for (SearchResult rentalCar : rentalCars) {
+            searchResults.add(
+                    new SearchResult(
+                            rentalCar.getCity(),
+                            "rental_car",
+                            rentalCar.getTitle()
+                    )
+            );
+        }
+        environment.jersey().register(new SearchResource(searchResults));
     }
-
 }
---
+++ src/main/java/com/skyscanner/Search.java
@@ -0,0 +1,17 @@
+package com.skyscanner;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Search {
+
+    @JsonProperty
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
